62bc35e2eeaec016efd1d6cd28bf1bb105799f43
Jordan Choo <jordan.choo@kogneta.com>
2023-12-02 08:17:40 -0500

    Created app workflow

---
+++ index.js
@@ -0,0 +1,25 @@
+// Import Packages
+
+// Set ENV variables
+
+// Get XML file
+
+// Parse XML file to JSON
+
+// Map Reduce (HTMl to Text + Parse Internal Links)
+
+// Map Reduce Vectorize
+
+// Upsert Pinecone Project
+
+// Upsert Index Project
+
+// Upsert Pinecone Project
+
+// Upsert Embeddings
+
+// Semantic Search URL
+
+// Remove already linked results
+
+// Output
