f4d7f9ca8068f273485365488897426da0d5f0d6
Jordan Choo <jordan.choo@kogneta.com>
2023-12-02 16:09:58 -0500

    Variablized target article ID and now isolating target article info in obj

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PINECONE_INDEX = process.env.PINECONE_INDEX;
 const PINECONE_ENVIRONMENT = process.env.PINECONE_ENVIRONMENT;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const ARTICLE_POSTS = process.env.ARTICLE_POSTS;
-const TARGET_ARTICLE_ID = process.env.TARGET_ARTICLE_ID;
+const TARGET_ARTICLE_ID = '705';
 const SCORE_THRESHOLD = process.env.SCORE_THRESHOLD;
 
 // Create OpenAI object
@@ -43,42 +43,47 @@ async function run() {
         };
     });
 
-    // Target a Pinecone index
-    const pineconeIndex = pinecone.index(PINECONE_INDEX);
+    // // Target a Pinecone index
+    // const pineconeIndex = pinecone.index(PINECONE_INDEX);
 
-    // OpenAI Vectorize + Push to Pinecone
-    for (let article = 0; article < formattedJson.length; article++) {
+    // // OpenAI Vectorize + Push to Pinecone
+    // for (let article = 0; article < formattedJson.length; article++) {
 
-        // Create embedding via OpenAI
-        let embedding = await openai.embeddings.create({
-            model: 'text-embedding-ada-002',
-            input: formattedJson[article].articleText,
-            encoding_format: 'float'
-        });
+    //     // Create embedding via OpenAI
+    //     let embedding = await openai.embeddings.create({
+    //         model: 'text-embedding-ada-002',
+    //         input: formattedJson[article].articleText,
+    //         encoding_format: 'float'
+    //     });
 
-        // Adde embedding data to JSON object
-        formattedJson[article].embedding = embedding
+    //     // Adde embedding data to JSON object
+    //     formattedJson[article].embedding = embedding
 
-        // Push embedding to Pinecone
-        await pineconeIndex.upsert([{
-            id: formattedJson[article]['wp:post_id']._text,
-            values: embedding.data[0].embedding
-        }]);
+    //     // Push embedding to Pinecone
+    //     await pineconeIndex.upsert([{
+    //         id: formattedJson[article]['wp:post_id']._text,
+    //         values: embedding.data[0].embedding
+    //     }]);
 
-        // Provide confirmation of saving
-        console.log(`Post ${formattedJson[article]['wp:post_id']._text} embedding saved to Pinecone`);
-    }
+    //     // Provide confirmation of saving
+    //     console.log(`Post ${formattedJson[article]['wp:post_id']._text} embedding saved to Pinecone`);
+    // }
 
-    // Save data to a JSON file
-    fs.writeFileSync('./output/article-embeddings.json', JSON.stringify(formattedJson));
+    // // Save data to a JSON file
+    // fs.writeFileSync('./output/article-embeddings.json', JSON.stringify(formattedJson));
 
     // Get matched opportunities from Pinecone
     let opps = await pinecone.index(PINECONE_INDEX).query({ topK: 50, id: '705'})
 
+    // Get Target Article Info
+    let targetArticleInfo = formattedJson.filter(function(target) {
+        return target['wp:post_id']._text === TARGET_ARTICLE_ID
+    })
+
     // Filter
     let filteredOpps = opps.matches.filter(function(opp) {
         // Remove target article & articles below the scoreThreshold
-        return opp.id !== '705' && opp.score >= 0.7;
+        return opp.id !== TARGET_ARTICLE_ID && opp.score >= 0.7;
     })
     
     // Merge Pinecone Results + WP Data
