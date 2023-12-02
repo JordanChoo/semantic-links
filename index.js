83c6ae03613a50e9acc15e94df4074a4b1543b34
Jordan Choo <jordan.choo@kogneta.com>
2023-12-02 09:00:51 -0500

    Importing XML articles and parsing to JSON along with making async

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { configurationOpenaAI, openAIApi } = require('openai');
 const fs = require('fs');
 const convertXml = require('xml-js');
+// const parseString = require('xml2js');
 const { convertHtml } = require('html-to-text');
 const { pinecone } = require('@pinecone-database/pinecone');
 
@@ -12,24 +13,30 @@ const PINECONE_ENVIRONMENT = process.env.PINECONE_ENVIRONMENT;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const ARTICLE_POSTS = process.env.ARTICLE_POSTS;
 
-// Get XML file
+async function run() {
+    // Get XML file
+    let articlesXml = await fs.readFileSync(ARTICLE_POSTS, 'utf8');
 
-// Parse XML file to JSON
+    // Parse XML file to JSON
+    let articlesJson = await convertXml.xml2js(articlesXml, {compact: false, spaces: 4, ignoreComment: true})
 
-// Map Reduce (HTMl to Text + Parse Internal Links)
+    // Map Reduce (HTMl to Text + Parse Internal Links)
 
-// Map Reduce Vectorize
+    // Map Reduce Vectorize
 
-// Upsert Pinecone Project
+    // Upsert Pinecone Project
 
-// Upsert Index Project
+    // Upsert Index Project
 
-// Upsert Pinecone Project
+    // Upsert Pinecone Project
 
-// Upsert Embeddings
+    // Upsert Embeddings
 
-// Semantic Search URL
+    // Semantic Search URL
 
-// Remove already linked results
+    // Remove already linked results
 
-// Output
+    // Output
+}
+
+run();
