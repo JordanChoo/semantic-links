04693397ce2e00896b6fd066225fea2d17670e1c
Jordan Choo <jordan.choo@kogneta.com>
2023-12-02 08:24:10 -0500

    Added ENV vars

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Import Packages
+require('dotenv').config();
+const { configurationOpenaAI, openAIApi } = require('openai');
+const fs = require('fs');
+const convertXml = require('xml-js');
+const { convertHtml } = require('html-to-text');
+const { pinecone } = require('@pinecone-database/pinecone');
 
 // Set ENV variables
+const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
+const PINECONE_ENVIRONMENT = process.env.PINECONE_ENVIRONMENT;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const ARTICLE_POSTS = process.env.ARTICLE_POSTS;
 
 // Get XML file
 
